dbb649b1eb2a7dfef06a4d7306676033f4b39768
riverundertheotter <rivercook2602@gmail.com>
2024-11-25 21:18:48 -0600

    Adding insertions

---
+++ data_insertions.sql
@@ -0,0 +1,93 @@
+INSERT INTO
+    COMPANY
+VALUES
+    (1, "The Company");
+
+INSERT INTO
+    ADDRESS
+VALUES
+    (
+        1,
+        77777,
+        "Boston",
+        "Massachussetts",
+        "200 West Park Ln"
+    ),
+    (2, 44444, "Sherman", "Texas", "1000 Houston St"),
+    (3, 99999, "Dallas", "Texas", "400 Lake Avenue"),
+    (
+        4,
+        11111,
+        "Queeb",
+        "Alabama",
+        "1600 Pennsylvania Ave"
+    );
+
+INSERT INTO
+    COMPANY_AFFILIATE
+VALUES
+    (1, "River", "Cook", 22, 'F', 2),
+    (2, "Jeremy", "Elbertson", 40, 'M', 1),
+    (3, "Michael", "Scott", 50, 'M', 3),
+    (4, "Hannah", "Hollow", 31, 'F', 4),
+    (5, "Jeb", "Bush", 50. 'M', 3);
+
+INSERT INTO
+    DEPARTMENT
+VALUES
+    (1, "Dep1"),
+    (2, "Dep2"),
+    (3, "Dep3");
+
+INSERT INTO
+    EMPLOYEE
+VALUES
+    (1, "Junior Software Engineer", 1),
+    (2, "Software Manager", 1),
+    (5, "Salesman", 2);
+
+INSERT INTO
+    POTENTIAL_EMPLOYEE
+VALUES
+    (3);
+
+INSERT INTO
+    CUSTOMER
+VALUES
+    (4, 5);
+
+INSERT INTO
+    MANAGES
+VALUES
+    (1, 2);
+
+INSERT INTO
+    PHONE_NUMBER
+VALUES
+    (1, 2143959240),
+    (2, 9032672339),
+    (3, 4697872234),
+    (4, 1111111111),
+    (5, 2222222222);
+
+INSERT INTO
+    SALARY
+VALUES
+    (1, 1, 01 -26 -2024, 2000),
+    (2, 2, 12 -26 -2023, 2500),
+    (3, 3, 2 -26 -2024, 1000);
+
+INSERT INTO
+    JOB_POS (1, "Example desc.", 02 -10 -2024, 1),
+    (2, "Example desc. 2", 10 -24 -2024);
+
+INSERT INTO
+    APPLICATION
+VALUES
+    (1, "", 3, 1),
+    (2, 2, "", 1);
+
+INSERT INTO INTERVIEW
+VALUES
+# I do not know the way to write a timestamp
+("", )
